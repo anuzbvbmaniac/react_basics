db2a6030dab5cb1c12533ca9a30d67b533fef850
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2021-01-04 00:19:02 +0545

    Changes: useRef Basics.

--- src/index.js
+++ src/index.js
@@ -1,56 +1,59 @@
-import React, { Component } from 'react';
+import React, {useRef} from 'react';
 import ReactDOM from 'react-dom';
 
-import DB from './db.json';
-
-import Header from "./components/header";
-import NewList from "./components/news_list";
-import Footer from "./components/footer";
-import Count from "./components/count";
+import './styles/style.css';
 
 
-import './styles/style.css'
-import Lifecycle from "./components/lifecycle";
+const App = () => {
 
-class App extends Component {
+    const textInput = useRef();
 
-    state = {
-        news: DB,
-        filteredNews: DB,
-        footerText: 'I am a happy footer.'
+    const triggerHandler = () => {
+        console.log(textInput.current.value);
     }
 
-    getKeyWords = (event) => {
-        let keywords = event.target.value;
-        let filteredNews = this.state.news.filter((item) => {
-            return item.title.indexOf(keywords) > -1;
-        });
-
-        this.setState({
-            filteredNews
-        });
-    }
-
-    render() {
-
-        const { footerText, filteredNews } = this.state
-
-        return (
-            <>
-                <Header
-                    keywords={this.getKeyWords}
-                />
-                <NewList
-                    news={filteredNews}
-                />
-                <Footer footerText={footerText}/>
-                <Lifecycle/>
-
-                <Count initialCount={0}/>
-            </>
-        );
-    }
-
+    return (
+        <>
+            <h1>Contact Form</h1>
+            <InputComponent ref={textInput}/>
+            <button onClick={triggerHandler}>
+                Trigger
+            </button>
+        </>
+    )
 }
+
+// Another Component ... Use React.forwardRef to pass references onto another component.
+const InputComponent = React.forwardRef((props, ref) => {
+    return (
+        <input type="text" ref={ref}/>
+    );
+});
+
+// Class Based Component
+// class App extends Component {
+//
+//     constructor(props) {
+//         super(props);
+//
+//         this.textInput = React.createRef();
+//     }
+//
+//     triggerHandler = () => {
+//         console.log(this.textInput.current.value);
+//     }
+//
+//     render() {
+//         return (
+//             <>
+//                 <h1>Contact Form</h1>
+//                 <input type="text" ref={this.textInput}/>
+//                 <button onClick={this.triggerHandler}>
+//                     Trigger
+//                 </button>
+//             </>
+//         );
+//     }
+// }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
